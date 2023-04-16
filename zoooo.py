b68bbd3aa1dc9161cc5a9e50d9e21c5720eccfdf
bardiya13 <bardisoltan@gmail.com>
2023-04-16 22:41:01 +0430

    neveshtan class baghe vahsh

--- zoooo.py
+++ zoooo.py
@@ -29,3 +29,30 @@ class ghafas:
     def getlistanimals(self):
         for i in self.animals:
             print(i)
+
+
+
+class baghvahsh:
+   def __init__(self,abadakvariomha,number_ghfas,tedadheyvanatghafas,tedadmahihayeakvariom,esmbaghevahsh):
+
+       self.akvariomlist=[]
+       for arz,tol,ertefa in abadakvariomha:
+           self.akvariomlist.append(akvariom(arz,tol,ertefa))#compositaion
+       self.tedadheyvanatghafas = tedadheyvanatghafas
+       self.tedadmahihayeakvariom = tedadmahihayeakvariom
+       self.esmbaghevahsh = esmbaghevahsh
+       self.number_ghfas=number_ghfas
+       self.ghafaslist=[]
+       for arzgh,tolgh,ertefagh in ghafasha:
+           self.ghafaslist.append(ghafas(arzgh,tolgh,ertefagh))#composition
+   def getlistghafas(self):
+       for i in self.ghafaslist:
+           print(i)
+   def getabadeakvarim(self):
+       for i in self.akvariomlist:
+           print(i)
+   def getnamezoo(self):
+       return self.esmbaghevahsh
+   def tedadheyvanat(self):
+       print((self.tedadmahihayeakvariom)+(self.tedadheyvanatghafas))
+
