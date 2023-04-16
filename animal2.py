91dd79af1ee3c06a3a5f190a52610dbc7837cf50
bardiya13 <bardisoltan@gmail.com>
2023-04-16 22:19:58 +0430

    neveshtan class pestandaran

--- animal2.py
+++ animal2.py
@@ -31,3 +31,42 @@ class mohredaran(ABC):
     @abstractmethod
     def bidar(self):
         pass
+
+
+class pestandaran(mohredaran):
+    def __init__(self, hagmemo, mahalezendegi, toleomr, toledom, dandan, tedadmohre, vazn, arz, sen):
+        super().__init__(tedadmohre, vazn, arz, sen)
+        self.hagmemo=hagmemo
+        self.__mahalezendegi=mahalezendegi
+        self.toleomr=toleomr
+        self.toledom=toledom
+        self.dandan=dandan
+
+    def harekat(self):
+        print("move")
+
+    def afzayeshsen(self):
+        self.sen+=1
+
+    def getarz(self):
+        return self.arz
+
+
+    def khab(self):
+        print("sleep zzz!")
+        self.iskhab=True
+
+    def bidar(self):
+        self.iskhab=False
+
+    def getmahalezendegi(self):
+        return f"i from {self.__mahalezendegi}"
+    def setmahalezendegi(self,mahalezendegi):
+        self.__mahalezendegi=mahalezendegi
+    #encapsilotion
+    def gettoleomr(self):
+        return f"sene ma {self.sen} hast va {self.toleomr-self.sen} sal digar zendam"
+    def getdandan(self):
+        return f"dandanhaye man {self.dandan} hast"
+    def getdom(self):
+        return f"andaze dome man {self.toledom} hast"
